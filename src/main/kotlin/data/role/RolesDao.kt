8422d9c28904545b3b072fb79faa67c519b90adc
stasbar <stasbar1995@gmail.com>
2018-03-15 23:35:05 +0100

    merge

--- src/main/kotlin/data/role/RolesDao.kt
+++ src/main/kotlin/data/role/RolesDao.kt
@@ -2,7 +2,6 @@ package data.role
 
 import data.DaoImpl
 import entrypoint.Database
-import model.Role
 
 interface RolesDao {
 
@@ -19,12 +18,30 @@ interface RolesDao {
 }
 
 class RolesDaoImpl(database: Database) : RolesDao, DaoImpl(database){
-
     companion object {
         const val SCHEMA = "CREATE TABLE Role (id INTEGER PRIMARY KEY, name VARCHAR(100) NOT NULL, description VARCHAR(255) NOT NULL)"
     }
 
-    fun getRolesForRoleId(userId: Long): Set<Role> {
-
+    override fun getRole(roleId: Long): RoleEntity? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun findRole(rolename: String): RoleEntity? {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun createRole(role: RoleEntity): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getAllRoles(): List<RoleEntity> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun deleteRole(roleId: Long) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+
+
 }
