c318a02be0803ec15e56e9d5085cfa712497e797
stasbar <stasbar1995@gmail.com>
2018-03-23 17:08:39 +0100

    add testLogin.sh script for testing purposes
    Took 1 minute

---
+++ testLogin.sh
@@ -0,0 +1,75 @@
+#!/bin/sh
+
+USERNAME=""
+PASSWORD=""
+METHOD=""
+COOKIEID=""
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    -u|--username|-l|--login)
+    USERNAME="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -p|--password)
+    PASSWORD="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -b|--cookie)
+	COOKIEID="$2"
+	shift
+	shift
+    ;;
+    *)    # unknown option
+	if [ -z "${METHOD}" ]; then
+		METHOD="$1"
+	elif [ -z "${USERNAME}" ]; then
+		USERNAME="$1"
+	elif [ -z "${PASSWORD}" ]; then
+		PASSWORD="$1"
+	else
+		POSITIONAL+=("$1") # save it in an array for later
+	fi
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+if [ -z "${METHOD}" ]; then
+	METHOD="login"
+fi
+if [ -z "${USERNAME}" ]; then
+	USERNAME="admin"
+fi
+if [ -z "${PASSWORD}" ]; then
+	PASSWORD="admin"
+fi
+
+case $METHOD in
+		"signup")
+	curl -X POST -v \
+	http://localhost:8080/$METHOD \
+	-H "Content-Type: application/x-www-form-urlencoded" \
+	-d "username=${USERNAME}&password=${PASSWORD}"
+	;;
+
+
+	"login")
+	curl -X GET -v \
+	--basic --user $USERNAME:$PASSWORD \
+	http://localhost:8080/$METHOD \
+	-H "Content-Type: application/x-www-form-urlencoded"
+	;;
+
+	*)
+	curl -X GET -v \
+	-b "SESSIONID=${COOKIEID}" \
+	http://localhost:8080/$METHOD \
+	-H "Content-Type: application/x-www-form-urlencoded"
+	;;
+esac
